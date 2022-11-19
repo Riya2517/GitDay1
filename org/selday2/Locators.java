2efd41f9a05e83930b5a7894f6feb62a848e8c7a
Riya2517 <janc1793@gmail.com>
2022-11-19 12:01:38 +0530

    push

---
+++ org/selday2/Locators.java
@@ -0,0 +1,14 @@
+package org.selday2;
+
+
+public class Locators {
+	public static void main(String[]args) {
+	    System.setProperty("webdriver.chrome.driver",
+	    		"D:\\New folder\\Practice\\selday2\\driver\\chromedriver.exe");
+	    
+	    
+	    
+	    
+	}
+
+}
